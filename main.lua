f311b44a11357235722b62f56b101a0335cd72cc
imAVALIDSkidderandihateeztorters <kadenburgoon1@gmail.com>
2025-06-28 14:21:12 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -29,9 +29,9 @@ local Window = Rayfield:CreateWindow({
    KeySettings = {
       Title = "Universal Menu",
       Subtitle = "Key System",
-      Note = "The Key Is PIZZA", -- Use this to tell the user how to get a key
+      Note = "The Key Is PIZZA ALL CAPS", -- Use this to tell the user how to get a key
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
       Key = {"PIZZA"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
