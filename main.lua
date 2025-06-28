b7ae9741cae9a523ad1a7c5946da83f8cc7cd4d9
imAVALIDSkidderandihateeztorters <kadenburgoon1@gmail.com>
2025-06-28 14:41:47 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -29,7 +29,7 @@ local Window = Rayfield:CreateWindow({
    KeySettings = {
       Title = "Universal Menu",
       Subtitle = "Key System",
-      Note = "The Key Is PIZZA", -- Use this to tell the user how to get a key
+      Note = "The Key Is PIZZA ALL CAPS", -- Use this to tell the user how to get a key
       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
       SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
@@ -38,11 +38,11 @@ local Window = Rayfield:CreateWindow({
 })
 
 local MainTab = Window:CreateTab("Player", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Main")
+local MainSection = MainTab:CreateSection("Main CANT TURN OFF")
 
 
 local EspTab = Window:CreateTab("Esp", nil) -- Title, Image
-local EspSection = EspTab:CreateSection("Esp")
+local EspSection = EspTab:CreateSection("Esp CANT TURN OFF")
 
 Rayfield:Notify({
    Title = "Script Executed Successfully!",
