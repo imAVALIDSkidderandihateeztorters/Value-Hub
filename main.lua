3e654e49aa3cc2c3a14f035f38b5c8d7beea5387
imAVALIDSkidderandihateeztorters <kadenburgoon1@gmail.com>
2025-09-27 12:01:25 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2096,49 +2096,8 @@ LocalPlayer.CharacterAdded:Connect(function(character)
 	handleTeleport(character, respawnTeleports)
 end)
 
-	do
+
 		Tab3:AddButton("Reset Player", function()
-
-						getgenv().Players = game:GetService'Players';
-getgenv().Host = Players.LocalPlayer;
-getgenv().ContextService = game:GetService'ContextActionService';
-
-
-
-local Method = 'Enum' -- You can change it to 'Enum, Health, Torso, Head, Joints' -- > DM ME @killserver IF YOU KNOW OF ANY MORE METHODS I CAN ADD < --
-
-function KillCharacter(Type:string)
-    if Host and Host.Character then
-        if Type == 'Enum' then
-            Host.Character:FindFirstChild'Humanoid':ChangeState(Enum.HumanoidStateType.Dead)
-        elseif Type == 'Health' then
-            Host.Character:FindFirstChild'Humanoid'.Health = 0
-        elseif Type == 'Torso' then
-            if Host.Character:FindFirstChild'Torso' or Host.Character:FindFirstChild'Humanoid'.RigType == Enum.HumanoidRigType.R6 then
-                Host.Character:WaitForChild'Torso':Destroy()
-            elseif Host.Character:FindFirstChild'UpperTorso' or Host.Character:FindFirstChild'Humanoid'.RigType == Enum.HumanoidRigType.R15 then
-                Host.Character:WaitForChild'UpperTorso':Destroy()
-            end
-        elseif Type == 'Head' then
-            Host.Character:FindFirstChild'Head':Destroy()
-        elseif Type == 'Joints' then
-            Host.Character:BreakJoints()
-        end
-    end
-end
-
-function Reset(Type:string, Properties:EnumItem)
-    if Type == 'Kill' then
-        if not Properties or Properties == Enum.UserInputState.Begin then
-            KillCharacter(Method)
-        end
-    end
-end
-
-ContextService:BindAction('Kill', Reset, true)
-
-
-	
 
 
 
