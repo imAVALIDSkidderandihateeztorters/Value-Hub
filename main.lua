40e2962f5662bdb16b94c0abfa990f1852635e16
imAVALIDSkidderandihateeztorters <kadenburgoon1@gmail.com>
2025-09-27 11:29:58 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2023,7 +2023,7 @@ function library:AddWindow(title, options)
 end
 
 do -- Example UI
-	local Window = library:AddWindow("ArenaRoyale", {
+	local Window = library:AddWindow("Arena Royale", {
 		main_color = Color3.fromRGB(41, 74, 122),
 		min_size = Vector2.new(500, 600),
 		toggle_key = Enum.KeyCode.RightShift,
